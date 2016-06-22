8f4980ce0749f35c17a98f2e44e603031d5e9b90
AleksanderLidtke <alek_l@onet.eu>
2016-06-22 13:38:38 +0100

    Started to convert from unicode to str when creating Articles.

--- GoogleScholarSearch.py
+++ GoogleScholarSearch.py
@@ -226,7 +226,7 @@ class GoogleScholarSearchEngine:
                         print "-"*10
                     
                     " Save the results. "
-                    results.append( Article.Article(pubTitle,map(str,pubAuthors.split(',')),pubJournalYear,pubJournalName,tagList=searchTerms,abstract=pubAbstract) )
+                    results.append( Article.Article(pubTitle.encode('utf-8'),map(lambda x: x.encode('utf-8'),pubAuthors.split(',')),pubJournalYear,pubJournalName.encode('utf-8'),tagList=searchTerms,abstract=pubAbstract.encode('utf-8')) )
                     # All the URLs.
                     results[-1].fullURL = fullURL
                     results[-1].pubURL = pubURL
